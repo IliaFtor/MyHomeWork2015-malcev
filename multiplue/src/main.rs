61b2cddb4bc993e0d5f3d987334841beb9a52069
IliaFtor <141063123+IliaFtor@users.noreply.github.com>
2024-05-27 12:50:25 +0300

    Add files via upload

--- multiplue/src/main.rs
+++ multiplue/src/main.rs
@@ -1,12 +1,7 @@
-fn main() {
-    let start = 1;
-    let end = 100_000;
-    let num_threads = 4;
+use rayon::prelude::*;
+use std::time::Instant;
 
-    let primes = find_primes_in_range(start, end, num_threads);
-    println!("Found {} prime numbers.", primes.len());
-}
-
+// Функция для проверки, является ли число простым
 fn is_prime(n: u64) -> bool {
     if n <= 1 {
         return false;
@@ -27,10 +22,23 @@ fn is_prime(n: u64) -> bool {
     true
 }
 
+// Функция для поиска простых чисел в заданном диапазоне
 fn find_primes_in_range(start: u64, end: u64, num_threads: usize) -> Vec<u64> {
-    let range: Vec<u64> = (start..=end).collect();
     let pool = rayon::ThreadPoolBuilder::new().num_threads(num_threads).build().unwrap();
     pool.install(|| {
-        range.into_par_iter().filter(|&n| is_prime(n)).collect()
+        (start..=end).into_par_iter().filter(|&n| is_prime(n)).collect()
     })
+}
+
+fn main() {
+    let start = 1;
+    let end = 100_000;
+    let thread_counts = [1, 2, 3, 4,5,6,7,8];
+
+    for &num_threads in &thread_counts {
+        let start_time = Instant::now();
+        let primes = find_primes_in_range(start, end, num_threads);
+        let duration = start_time.elapsed();
+        println!("Threads: {}, Time: {:?}, Primes found: {}", num_threads, duration, primes.len());
+    }
 }
